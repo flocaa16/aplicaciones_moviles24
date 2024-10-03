9f32a09f8423f5c598df5ef87568d5ed726e2c7a
“flocaa16” <flcamposa@udd.cl>
2024-10-03 16:11:42 -0300

    03/10/2024. Florencia Campos subió evaluación Flutter

--- f_campos_octubre/f_campos_octubre/lib/main.dart
+++ f_campos_octubre/f_campos_octubre/lib/main.dart
@@ -10,172 +10,118 @@ class MainApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData.light().copyWith(
-        appBarTheme: const AppBarTheme(
-          backgroundColor: Colors.black,
-          foregroundColor: Colors.white,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(
+          seedColor: const Color.fromARGB(255, 255, 255, 255),
         ),
+        elevatedButtonTheme: ElevatedButtonThemeData(
+            style: ElevatedButton.styleFrom(
+          backgroundColor: const Color.fromARGB(255, 230, 230, 230),
+          foregroundColor: const Color.fromARGB(255, 14, 19, 23),
+          elevation: 0,
+        )),
       ),
       home: Scaffold(
-        appBar: AppBar(title: const Text('Inicio'), actions: const [
-          Icon(Icons.notifications_outlined),
-          Padding(
-            padding: EdgeInsets.symmetric(horizontal: 16.0),
-            child: Icon(Icons.settings_outlined),
-          )
-        ]),
+        appBar: AppBar(
+          leading: const Icon(Icons.arrow_back),
+          title: const Text('Kundenbewertungen'),
+        ),
         body: ListView(
-          padding: const EdgeInsets.all(16.0),
           children: [
-            //primera card
-            Card(
-              elevation: 4.0,
-              margin: const EdgeInsets.all(8.0),
-              child: Column(
-                crossAxisAlignment: CrossAxisAlignment.center, // Determina en qué lugar en ele eje alinean o comienzan los elementos 
-                // start: extremo izq; end: extremo derecho; center: al centro; strecht: tratará de ocupar todo el ancho disponible
-                children: [
-                  Container(
-                    height: 200,
-                    color: const Color.fromARGB(255, 197, 197, 197),
-                    width: double.infinity,
-                  ),
-                  const ListTile( // Para crear listas de items
-                    title: Text('Título de la Card 1'),
-                    subtitle: Text('Este es el subtitle'),
-
-                  ),
-                  Padding(
-                    padding: const EdgeInsets.all(16.0),
-                    child: ElevatedButton(
-                      onPressed: () {
-                        // Acción cuando se presiona el botón
-                      },
-                      child: const Text('ver más'),
-                    ),
-                  ),
-                ],
+            Container(
+              height: 170.0,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 162, 6, 6),
+              //child: const Text(''),
+            ),
+            const SizedBox(
+              height: 22.0,
+            ),
+            const Padding(
+                padding: EdgeInsets.symmetric(horizontal: 16.0),
+                child: Text('Nach Standard sortieren',
+                    style: TextStyle(fontWeight: FontWeight.bold))),
+            const SizedBox(
+              height: 22.0,
+            ),
+            Container(
+              height: 200.0,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 162, 6, 6),
+            ),
+            const SizedBox(
+              height: 12.0,
+            ),
+            ListTile(
+              title: const Text(
+                'Das Feedback wurde teilweise automatisch übersetzt.',
+                style: TextStyle(fontSize: 10, fontWeight: FontWeight.normal),
+              ),
+              trailing: ElevatedButton(
+                onPressed: () {},
+                child: const Text(
+                  'Original zeigen',
+                  style: TextStyle(fontSize: 12),
+                ),
               ),
             ),
-            //Espacio entre las cards
             const SizedBox(
-              height: 16.0,
+              height: 12.0,
             ),
-            Row(
-              children: [
-                Expanded(
-                  child: Card(
-                    elevation: 4.0,
-                    margin: const EdgeInsets.only(left: 8.0, bottom: 16.0), // Margin permite determinar cuánto espacio externo tendrá un widget
-                    // Utiliza EdgeInsets para definir el epacio por cada lado.
-                    // margin: const EdgeInsets.formLTRB(Left, Top, Right, Bottom),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Container(
-                          height: 150,
-                          color: const Color.fromARGB(255, 197, 197, 197),
-                          width: double.infinity,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(16.0),
-                          child: const Text(
-                            'Titulo de la Card',
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              //Acción cuando se presiona el botón
-                            },
-                            child: const Text('Ver más'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-
-                //Segunda al lado
-                Expanded(
-                  child: Card(
-                    elevation: 4.0,
-                    margin: const EdgeInsets.only(left: 8.0, bottom: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Container(
-                          height: 150,
-                          color: const Color.fromARGB(255, 197, 197, 197),
-                          width: double.infinity,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(16.0),
-                          child: const Text(
-                            'Titulo de la Card',
-                            style: TextStyle(
-                                fontSize: 15, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              //Acción cuando se presiona el botón
-                            },
-                            child: const Text('Ver más'),
-                          ),
-                        ),
-                      ],
-                    ),
-                  ),
-                ),
-              ],
+            Container(
+              height: 100.0,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 162, 6, 6),
             ),
             const SizedBox(
-              height: 16.0,
+              height: 12.0,
             ),
-            Expanded(
-                  child: Card(
-                    elevation: 4.0,
-                    margin: const EdgeInsets.only(left: 8.0, bottom: 16.0),
-                    child: Column(
-                      crossAxisAlignment: CrossAxisAlignment.stretch,
-                      children: [
-                        Container(
-                          height: 150,
-                          color: const Color.fromARGB(255, 197, 197, 197),
-                          width: double.infinity,
-                        ),
-                        Container(
-                          padding: const EdgeInsets.all(16.0),
-                          child: const Text(
-                            'Titulo de la Card',
-                            style: TextStyle(
-                                fontSize: 18, fontWeight: FontWeight.bold),
-                          ),
-                        ),
-                        Padding(
-                          padding: const EdgeInsets.all(16.0),
-                          child: ElevatedButton(
-                            onPressed: () {
-                              //Acción cuando se presiona el botón
-                            },
-                            child: const Text('Ver más'),
-                          ),
-                        ),
-                      ],
+            Container(
+              height: 100.0,
+              width: double.infinity,
+              color: const Color.fromARGB(255, 162, 6, 6),
+            ),
+            const SizedBox(
+              height: 8.0,
+            ),
+            Container(
+              height: 100.0,
+              color: Colors.transparent,
+              child: SingleChildScrollView(
+                scrollDirection: Axis.horizontal,
+                child: Row(
+                  children: [
+                    Container(
+                      width: 100,
+                      margin: const EdgeInsets.only(right: 12.0),
+                      color: const Color.fromARGB(255, 184, 11, 97),
                     ),
-                  ),
+                    Container(
+                      width: 100,
+                      margin: const EdgeInsets.only(right: 12.0),
+                      color: const Color.fromARGB(255, 184, 11, 97),
+                    ),
+                    Container(
+                      width: 100,
+                      margin: const EdgeInsets.only(right: 12.0),
+                      color: const Color.fromARGB(255, 184, 11, 97),
+                    ),
+                    Container(
+                      width: 100,
+                      margin: const EdgeInsets.only(right: 12.0),
+                      color: const Color.fromARGB(255, 184, 11, 97),
+                    ),
+                  ],
                 ),
+              ),
+            ),
+               const SizedBox(
+              height: 12.0,
+            ),
           ],
+          //Children
         ),
       ),
     );
   }
 }
-
